4fc7cd7ba256b606631e9d771c8a2a4a0f553bc5
Magnus <bcict20070@ttu.edu.gh>
2025-03-12 12:46:32 +0000

    feat: implement post component with dynamic content fetching and styling

--- src/app/core/Pages/post/post.component.ts
+++ src/app/core/Pages/post/post.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
+import { ContentfulService } from '../../shared/services/contentful.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
 export class PostComponent {
   
   
-  constructor() { }
+  constructor(private contentfulService: ContentfulService, private routes: ActivatedRoute) { }
 
+  foodPost$: Observable<any> | undefined
+
+  ngOnInit() {
+    this.routes.params.subscribe(params => {
+      const id = params['id'];
+    
+      this.foodPost$ = this.contentfulService.getEntry(id);
+    });
+   }
 
 }
 
